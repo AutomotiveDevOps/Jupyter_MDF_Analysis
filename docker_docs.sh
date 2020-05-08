dc6f8a91c92a62ea18bfa7ffd5799a57472f459c
user1 <8600k>
2020-05-08 15:51:18 -0400

    Move to repo2docker for using online

--- docker_docs.sh
+++ docker_docs.sh
@@ -4,5 +4,5 @@ echo ${SCRIPT}
 export DIR=`dirname ${SCRIPT}`
 echo ${DIR}
 
-docker build --tag jupyter_mdf_analysis:dev .
+docker build --file Dockerfile-alternate --tag jupyter_mdf_analysis:dev .
 docker run --workdir /home/jovian --interactive --tty --volume ${DIR}:/home/jovian jupyter_mdf_analysis_jupyter make docs
